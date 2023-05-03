1544ce1dce8ba4432f2c199a1ae36c826b85130e
Anatoly <kolganovpix@yandex.ru>
2023-05-03 22:56:41 +0500

    Подключает модель

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import FilterView from './view/filter-view.js';
 import EventPresenter from './presenter/event-presenter.js';
 import { render } from './render.js';
+import EventsModel from './model/events-model.js';
 
 const siteMainElement = document.querySelector('main');
 const siteHeaderElement = document.querySelector('header');
 const siteFiltersElement = siteHeaderElement.querySelector('.trip-controls__filters');
 const siteEventsElement = siteMainElement.querySelector('.trip-events');
-const eventPresenter = new EventPresenter({eventContainer: siteEventsElement});
+const eventsModel = new EventsModel();
+const eventPresenter = new EventPresenter({eventContainer: siteEventsElement, eventsModel});
+
 
 render(new FilterView(), siteFiltersElement);
 
--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -8,18 +8,21 @@ import { render } from '../render.js';
 export default class EventPresenter {
   eventListComponent = new EventListView();
 
-  constructor({eventContainer}){
+  constructor({eventContainer, eventsModel}){
     this.eventContainer = eventContainer;
+    this.eventsModel = eventsModel;
   }
 
   init(){
+    this.events = [...this.eventsModel.getEvents()];
+
     render(new SortView(), this.eventContainer);
     render(this.eventListComponent, this.eventContainer);
     render(new EventEditView(), this.eventListComponent.getElement());
     render(new EventNewView(), this.eventListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new EventView(), this.eventListComponent.getElement());
+    for (let i = 0; i < this.events.length; i++) {
+      render(new EventView(this.events[i]), this.eventListComponent.getElement());
     }
 
   }
