97cad028c86ee6ad1b06b895610758e8b8e5613f
Anatoly <kolganovpix@yandex.ru>
2023-06-06 03:16:11 +0500

    Изменяет view фильтров

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,27 +1,31 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createFilterTemplate(){
+function createFilterItem(filter, isChecked){
+  return `
+    <div class="trip-filters__filter">
+      <input
+        id="filter-${filter.type}"
+        class="trip-filters__filter-input  visually-hidden"
+        type="radio"
+        name="trip-filter"
+        value="${filter.type}"
+        ${isChecked ? 'checked' : ''}
+        ${(filter.hasEvents) ? '' : 'disabled'}
+        >
+      <label
+        class="trip-filters__filter-label"
+        for="filter-${filter.type}"
+      >
+      ${filter.type}
+      </label>
+    </div>
+  `;
+}
+
+function createFilterTemplate({filters}){
   return (
     `<form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-        <label class="trip-filters__filter-label" for="filter-present">Present</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
+      ${filters.map((filter, index) => createFilterItem(filter, index === 0)).join('')}
 
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`
@@ -29,7 +33,14 @@ function createFilterTemplate(){
 }
 
 export default class FilterView extends AbstractView {
+  #filters = [];
+
+  constructor(filters){
+    super();
+    this.#filters = filters;
+  }
+
   get template(){
-    return createFilterTemplate();
+    return createFilterTemplate({filters: this.#filters});
   }
 }
