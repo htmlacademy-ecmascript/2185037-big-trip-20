74790f3e60643c249ba4c8b4482eeb130be44345
Anatoly <kolganovpix@yandex.ru>
2023-06-21 23:15:22 +0500

    Меняет выводимый текст

--- src/view/event-list-empty-view.js
+++ src/view/event-list-empty-view.js
@@ -3,13 +3,12 @@ import { FilterType } from '../const.js';
 
 const EmptyEventTextType = {
   [FilterType.EVERYTHING]: 'Click New Event to create your first point',
-  [FilterType.FUTURE]: 'There are no past events now',
-  [FilterType.PAST]: 'There are no present events now',
-  [FilterType.PRESENT]:'There are no future events now'
+  [FilterType.FUTURE]: 'There are no future events now',
+  [FilterType.PAST]: 'There are no past events now',
+  [FilterType.PRESENT]:'There are no present events now'
 };
 
 function createEventListTemplate(filterType){
-  console.log(filterType);
   return `<p class="trip-events__msg">${EmptyEventTextType[filterType]}</p>`;
 }
 
